f7487771593edb879db4f9f5e80730b5a8be870c
rosasc <rosasc@D017LAB-20>
2016-11-14 12:28:23 -0500

    Added Identity

---
+++ src/MusicFall2016/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace MusicFall2016
+{
+    public class ApplicationUser : IdentityUser
+
+    {
+    }
+}
--- src/MusicFall2016/Startup.cs
+++ src/MusicFall2016/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,6 +36,11 @@ namespace MusicFall2016
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+       .AddEntityFrameworkStores<MusicDbContext>()
+       .AddDefaultTokenProviders();
+
             // Add framework services.
             services.AddApplicationInsightsTelemetry(Configuration);
 
